0fd468598fe58aa49c226b1d03603b05138c9d0e
CodeMonkeyReport <michael.alexander.geyer@gmail.com>
2018-12-04 19:55:26 -0600

    Cleaned up train and test scripts

--- test.py
+++ test.py
@@ -17,10 +17,11 @@ letters = [chr(i) for i in range(ord('a'), ord('z')+1)]
 
 model = cnn.build_model()
 (X, Y) = cnn.get_data(letters, data_folder)
-model.load_weights('model_v2_clean_dropout_500.h5')
+
+# Latest version of the model is input here, several versions can be found in the models folder.
+model.load_weights('model.h5')
 
 res = model.evaluate([X[0].reshape(-1, height, width_emg, 1), X[1].reshape(-1, height, width_acl, 1)], Y, verbose=1)
-
 P = model.predict([X[0].reshape(-1, height, width_emg, 1), X[1].reshape(-1, height, width_acl, 1)], verbose=1)
 
 truth = np.array(Y)
--- train.py
+++ train.py
@@ -4,17 +4,18 @@ width_emg = 8
 width_acl = 4
 height = 250
 data_folder = 'data-v2'
-epochs = 1000
+epochs = 500
 
+# Get the model
 model = cnn.build_model()
-
 
 # letters from a to z lower case
 letters = [chr(i) for i in range(ord('a'), ord('z')+1)]
 
+# read data
 (X, Y) = cnn.get_data(letters, data_folder)
 
 model.summary()
-#model.load_weights('model_v2_clean_dropout_1000.h5')
+# Data needs to be reshaped to fit model parameters
 model.fit([X[0].reshape(-1, height, width_emg, 1), X[1].reshape(-1, height, width_acl, 1)], Y, epochs=epochs)
-model.save('model_v2_clean_dropout_500.h5')
+model.save('model.h5') 
